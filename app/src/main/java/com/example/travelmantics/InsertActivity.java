41932d75ecd6745717a8aa8e8f3aed3325d38564
mbatia muniu <mbatiamnew@gmail.com>
2020-07-12 00:01:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -0,0 +1,25 @@
+package com.example.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu,menu);
+        return true;
+    }
+}
+
+
